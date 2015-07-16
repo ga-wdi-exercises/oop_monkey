699821570a9b751f52fb3ace2f1265dad6564865
Adam Bray <adam.bray@yale.edu>
2015-07-16 14:12:24 -0400

    added bonus solution

--- lib/monkey.rb
+++ lib/monkey.rb
@@ -2,13 +2,13 @@ class Monkey
   attr_reader :name, :species, :foods_eaten
 
   def initialize(name, species)
-    @name = name
+    @name = name.capitalize
     @species = species
     @foods_eaten = []
   end
 
   def eat(food)
-    @foods_eaten.push(food)
+    @foods_eaten.push(food) unless ['a', 'e', 'i', 'o', 'u'].include?(food[0].downcase)
   end
 
   def introduce
