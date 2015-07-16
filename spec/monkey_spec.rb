edfb24a416306ba598961a7c272b198c20555b7f
Adam Bray <adam.bray@yale.edu>
2015-07-16 15:14:39 -0400

    updated readme

--- spec/monkey_spec.rb
+++ spec/monkey_spec.rb
@@ -1,19 +1,26 @@
 require_relative '../lib/monkey'
 
+# this part just tells RSpec and people what class this test is for
 describe Monkey do
 
+  # each `it` tests a specific functionality of our Monkey objects
   it "has a name" do
+    # often we start a test by setting up a specific state
     adam = Monkey.new("Adam", "Spider Monkey")
+
+    # expect runs the code we pass in, and compares the result to the eq() part
     expect(adam.name).to eq("Adam")
   end
 
   it "has a species" do
     andy = Monkey.new("Andy", "Cool Monkey")
+
     expect(andy.species).to eq("Cool Monkey")
   end
 
   it "starts with no foods eaten" do
     adrian = Monkey.new("Adrian", "White-headed Capuchin")
+
     expect(adrian.foods_eaten).to eq([])
   end
 
@@ -21,6 +28,8 @@ describe Monkey do
     matt = Monkey.new("Matt", "Mandrill")
     matt.eat("banana")
     matt.eat("PB&J")
+
+    # we can make more general expectations, like expect(this_array) to include something
     expect(matt.foods_eaten).to include("banana")
     expect(matt.foods_eaten).to include("PB&J")
   end
@@ -54,6 +63,7 @@ describe Monkey do
   #   robin.eat("pudding")
   #   robin.eat("chocolate")
   #
+  #   # we only expect to see non-vowel foods in the list of foods_eaten
   #   expect(robin.foods_eaten).to eq(["pudding", "chocolate"])
   # end
 
